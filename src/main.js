cce0e983b5848a60deab3be122f03d84c2c7e706
Cates Key <461354358@qq.com>
2021-10-28 10:12:58 +0800

    fix: 引入loading错误

--- src/main.js
+++ src/main.js
@@ -1,26 +1,11 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler'
 import './tailwind.css'
 import App from './App.vue'
 import { router } from './routes.js'
-import {
-  ElAlert,
-  ElPagination,
-  ElTable,
-  ElTableColumn,
-  ElButton,
-  ElImage,
-  ElInput,
-  ElForm,
-  ElFormItem,
-  ElTag,
-  ElLoading,
-  ElLoadingDirective,
-} from 'element-plus'
+import { ElAlert, ElButton, ElDivider, ElForm, ElFormItem, ElImage, ElInput, ElLoading, ElPagination, ElTable, ElTableColumn, ElTag } from 'element-plus'
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
-
-
 
 app.use(ElTable)
 app.use(ElTableColumn)
@@ -33,8 +18,14 @@ app.use(ElTag)
 app.use(ElPagination)
 app.use(ElAlert)
 app.use(ElLoading)
-app.use(ElLoadingDirective)
+app.use(ElDivider)
 
 app.use(router)
 app.use(createPinia())
+
+// app.directive('test', (...params) => {
+//   console.log('params', params)
+//   params[2].children.push
+// })
+
 app.mount('#app')
