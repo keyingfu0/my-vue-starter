948d02e5b61d75e44524ba3a157010f992578462
Cates Key <461354358@qq.com>
2021-11-26 14:18:10 +0800

    feat: 引入 babel

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue/dist/vue.esm-bundler'
 import './tailwind.css'
 import App from './App.vue'
 import { router } from './routes.js'
-import { ElAlert, ElButton, ElDivider, ElForm, ElFormItem, ElImage, ElInput, ElLoading, ElPagination, ElTable, ElTableColumn, ElTag } from 'element-plus'
 import { createPinia } from 'pinia'
 
 import XEUtils from 'xe-utils'
@@ -108,19 +107,6 @@ function useTable(app) {
 
 const app = createApp(App)
 app.use(useTable)
-
-app.use(ElTable)
-app.use(ElTableColumn)
-app.use(ElButton)
-app.use(ElImage)
-app.use(ElInput)
-app.use(ElForm)
-app.use(ElFormItem)
-app.use(ElTag)
-app.use(ElPagination)
-app.use(ElAlert)
-app.use(ElLoading)
-app.use(ElDivider)
 
 app.use(router)
 app.use(createPinia())
--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,48 @@ import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 import { resolve } from 'path'
 import styleImport from 'vite-plugin-style-import'
+import babel from '@rollup/plugin-babel'
 
 export default defineConfig({
-  plugins: [vue(), ElementPlus(),
+  plugins: [
+    vue(),
+    ElementPlus(),
     styleImport({
       libs: [
         {
           libraryName: 'vxe-table',
           esModule: true,
           resolveComponent: (name) => `vxe-table/es/${name}`,
-          resolveStyle: (name) => `vxe-table/es/${name}/style.css`
-        }
-      ]
-    })
+          resolveStyle: (name) => `vxe-table/es/${name}/style.css`,
+        },
+      ],
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+          },
+        ],
+      ],
+      extensions: ['.ts', '.tsx', '.js', '.vue'],
+      exclude: 'node_modules/**',
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        [
+          'import',
+          {
+            libraryName: 'ant-design-vue',
+            libraryDirectory: 'es',
+            style: 'css',
+          },
+        ],
+        '@babel/plugin-proposal-optional-chaining',
+        // '@babel/plugin-proposal-nullish-coalescing-operator',
+      ],
+    }),
   ],
   resolve: {
     alias: {
